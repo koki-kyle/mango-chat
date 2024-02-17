3e934a0ea8c134a6526f50666c3b7c0609deed1f
Dttrx <126334326+dttrx@users.noreply.github.com>
2024-02-17 15:19:17 +0100

    feat: implement handleClient method in TCPServer

--- src/main/java/io/koki/mangochat/networking/TCPServer.java
+++ src/main/java/io/koki/mangochat/networking/TCPServer.java
@@ -1,5 +1,11 @@
 package io.koki.mangochat.networking;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class TCPServer implements Server {
     private volatile boolean running = false;
 
@@ -16,5 +22,27 @@ public class TCPServer implements Server {
     @Override
     public boolean isRunning() {
         return running;
+    }
+
+    private void handleClient(Socket clientSocket) {
+        String username = String.format("%s:%d", clientSocket.getInetAddress().getCanonicalHostName(), clientSocket.getPort());
+
+        new Thread(() -> {
+            try (
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                    PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true)
+            ) {
+                String message;
+
+                while ((message = reader.readLine()) != null) {
+                    System.out.printf("received message from %s > %s%n", username, message);
+                    
+                    writer.printf("server received your message: %s%n", message);
+                }
+            } catch (IOException ignore) {
+            } finally {
+                System.out.printf("client disconnected: %s%n", username);
+            }
+        }).start();
     }
 }
