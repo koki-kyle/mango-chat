78a48afe29b3d1ca68f2f2dd243b8a68cb8c4752
Dttrx <126334326+dttrx@users.noreply.github.com>
2024-02-17 13:42:33 +0100

    feat: implement Server interface in TCPServer and UDPServer

--- src/main/java/io/koki/mangochat/networking/TCPServer.java
+++ src/main/java/io/koki/mangochat/networking/TCPServer.java
@@ -1,4 +1,13 @@
 package io.koki.mangochat.networking;
 
-public class TCPServer {
+public class TCPServer implements Server {
+    @Override
+    public void startServer(int port) {
+        throw new RuntimeException("not implemented");
+    }
+
+    @Override
+    public void stopServer() {
+        throw new RuntimeException("not implemented");
+    }
 }
