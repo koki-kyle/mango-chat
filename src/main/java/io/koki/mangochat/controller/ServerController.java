724b71542844f8884d36a4feafef064ddd7a9d05
Dttrx <126334326+dttrx@users.noreply.github.com>
2024-02-17 14:00:19 +0100

    feat: implement initial ServerController

--- src/main/java/io/koki/mangochat/controller/ServerController.java
+++ src/main/java/io/koki/mangochat/controller/ServerController.java
@@ -1,4 +1,22 @@
 package io.koki.mangochat.controller;
 
+import io.koki.mangochat.model.DatabaseManager;
+import io.koki.mangochat.networking.Server;
+
 public class ServerController {
+    private final Server server;
+    private final DatabaseManager databaseManager;
+
+    public ServerController(Server server, DatabaseManager databaseManager) {
+        this.server = server;
+        this.databaseManager = databaseManager;
+    }
+
+    public void startServer(int port) {
+        server.startServer(port);
+    }
+
+    public void stopServer() {
+        server.stopServer();
+    }
 }
