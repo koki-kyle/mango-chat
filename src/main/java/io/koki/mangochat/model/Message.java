7f4af4755ceafd996c5b621c8ecff0c8d0328adf
Dttrx <126334326+dttrx@users.noreply.github.com>
2024-02-20 19:23:30 +0100

    feat: implement Message class with lombok @Data annotation

--- src/main/java/io/koki/mangochat/model/Message.java
+++ src/main/java/io/koki/mangochat/model/Message.java
@@ -1,4 +1,16 @@
 package io.koki.mangochat.model;
 
-public class Message {
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+public class Message implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 10L;
+    private final User owner;
+    private final String body;
+    private final LocalDateTime timestamp;
 }
