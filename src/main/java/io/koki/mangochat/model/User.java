5ebca6e4e8226deb94c6114d4f2fd69f137403d0
Dttrx <126334326+dttrx@users.noreply.github.com>
2024-02-20 19:22:41 +0100

    feat: implement User class with lombok @Data and @AllArgsConstructor annotations

--- src/main/java/io/koki/mangochat/model/User.java
+++ src/main/java/io/koki/mangochat/model/User.java
@@ -1,4 +1,16 @@
 package io.koki.mangochat.model;
 
-public class User {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 20L;
+    private String username;
+    private String password;
 }
