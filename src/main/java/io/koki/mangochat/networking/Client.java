1d2fb5498904e28d32874aac330b4cbbb1ea09a8
dttrx <126334326+dttrx@users.noreply.github.com>
2024-02-21 13:13:15 +0100

    feat: add setUpServer and reintroduce disconnect methods in Client interface

--- src/main/java/io/koki/mangochat/networking/Client.java
+++ src/main/java/io/koki/mangochat/networking/Client.java
@@ -2,7 +2,11 @@ package io.koki.mangochat.networking;
 
 import io.koki.mangochat.model.User;
 
+import java.net.InetAddress;
+
 public interface Client {
+    boolean setUpServer(InetAddress address, int port);
     boolean authenticate(User user);
     void fetchMessages();
+    void disconnect();
 }
