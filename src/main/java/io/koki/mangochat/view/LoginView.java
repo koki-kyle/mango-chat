1a7d48855129a8e9896c232231484c5cdacce5a0
dttrx <126334326+dttrx@users.noreply.github.com>
2024-02-21 13:15:58 +0100

    feat: add setWindowClosingListener method to LoginView

--- src/main/java/io/koki/mangochat/view/LoginView.java
+++ src/main/java/io/koki/mangochat/view/LoginView.java
@@ -3,6 +3,7 @@ package io.koki.mangochat.view;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
 
 public class LoginView extends JFrame {
     private final JButton loginButton;
@@ -70,6 +71,10 @@ public class LoginView extends JFrame {
 
     public void setLoginButtonListener(ActionListener listener) {
         loginButton.addActionListener(listener);
+    }
+
+    public void setWindowClosingListener(WindowListener listener) {
+        this.addWindowListener(listener);
     }
 
     public void displayErrorMessage(String errorMessage) {
