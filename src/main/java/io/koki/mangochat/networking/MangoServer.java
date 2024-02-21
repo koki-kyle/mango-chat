02a36d5b565141704383db53f33dd6ff9ee6c90f
Dttrx <126334326+dttrx@users.noreply.github.com>
2024-02-21 19:26:31 +0100

    feat: add setUserAuthenticationPredicate method to MangoServer interface

--- src/main/java/io/koki/mangochat/networking/MangoServer.java
+++ src/main/java/io/koki/mangochat/networking/MangoServer.java
@@ -1,8 +1,15 @@
 package io.koki.mangochat.networking;
 
+import io.koki.mangochat.model.User;
+
+import java.util.function.Predicate;
+
 public interface MangoServer {
     void startServer(int port);
 
     void stopServer();
+
     boolean isRunning();
+
+    void setUserAuthenticationPredicate(Predicate<User> predicate);
 }
