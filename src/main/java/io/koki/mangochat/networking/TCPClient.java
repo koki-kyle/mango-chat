600cb130968dda247795ac347b8b32e765bc90e5
dttrx <126334326+dttrx@users.noreply.github.com>
2024-02-21 13:27:48 +0100

    feat: implement setUpServer and disconnect methods in TCPClient

--- src/main/java/io/koki/mangochat/networking/TCPClient.java
+++ src/main/java/io/koki/mangochat/networking/TCPClient.java
@@ -1,4 +1,48 @@
 package io.koki.mangochat.networking;
 
-public class TCPClient {
+import io.koki.mangochat.model.User;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class TCPClient implements Client {
+    private Socket serverSocket = null;
+
+    @Override
+    public boolean setUpServer(InetAddress address, int port) {
+        boolean successful = false;
+
+        try {
+            serverSocket = new Socket(address, port);
+
+            System.out.printf("TCP Client connected to %s:%d%n", address.getCanonicalHostName(), port);
+        } catch (IOException e) {
+            System.err.println("could not connect to the server: " + e.getMessage());
+        }
+
+        return successful;
+    }
+
+    @Override
+    public boolean authenticate(User user) {
+        return true;
+    }
+
+    @Override
+    public void fetchMessages() {
+        throw new RuntimeException("not implemented");
+    }
+
+    @Override
+    public void disconnect() {
+        try {
+            if (serverSocket != null && !serverSocket.isClosed()) {
+                serverSocket.close();
+            }
+        } catch (IOException ignore) {
+        } finally {
+            System.out.println("TCP client disconnected");
+        }
+    }
 }
